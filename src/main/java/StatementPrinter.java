cf02678015d1ec8e261b6d0e459be70ece281a52
KamaroK <kamarokevis@gmail.com>
2022-11-03 15:47:23 +0100

    create method totalAmount

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -1,6 +1,8 @@
 import java.text.NumberFormat;
 import java.util.*;
 
+import javax.management.loading.PrivateClassLoader;
+
 public class StatementPrinter {
 
 
@@ -13,25 +15,7 @@ public class StatementPrinter {
 
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
-
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
+      int thisAmount = totalAmount(perf, play);
 
       // add volume credits
       volumeCredits += Math.max(perf.audience - 30, 0);
@@ -40,13 +24,37 @@ public class StatementPrinter {
         volumeCredits += Math.floor(perf.audience / 5);
 
       // print line for this order
-      result.append(" l " + play.name + ": " + frmt.format(thisAmount / 100) + " (" + perf.audience + " seats)\n");
+      result.append("  " + play.name + ": " + frmt.format(thisAmount / 100) + " (" + perf.audience + " seats)\n");
       totalAmount += thisAmount;
     }
     result.append("Amount owed is " + frmt.format(totalAmount / 100) + "\n");
     result.append("You earned " + volumeCredits + " credits\n");
     
     return result;
+  }
+
+  private int totalAmount(Performance perf, Play play)
+  {
+    int thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+        default:
+          throw new Error("unknown type: ${play.type}");
+      }
+    return thisAmount;
   }
 
 }
