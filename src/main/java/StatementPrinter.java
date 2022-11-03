fb6f5143978bf84deeed5cd2214aa8a1f191672c
KamaroK <kamarokevis@gmail.com>
2022-11-03 15:55:14 +0100

    change variable name

--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -1,7 +1,6 @@
 import java.text.NumberFormat;
 import java.util.*;
 
-import javax.management.loading.PrivateClassLoader;
 
 public class StatementPrinter {
 
@@ -35,26 +34,26 @@ public class StatementPrinter {
 
   private int totalAmount(Performance perf, Play play)
   {
-    int thisAmount = 0;
+    int result = 0;
 
     switch (play.type) {
       case "tragedy":
-        thisAmount = 40000;
+        result = 40000;
         if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+          result += 1000 * (perf.audience - 30);
       }
       break;
       case "comedy":
-        thisAmount = 30000;
+        result = 30000;
         if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
+          result += 10000 + 500 * (perf.audience - 20);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * perf.audience;
         break;
         default:
           throw new Error("unknown type: ${play.type}");
       }
-    return thisAmount;
+    return result;
   }
 
 }
